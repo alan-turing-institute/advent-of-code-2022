eda9ec7173e1dcf29ae6c7774b8862076addc2e3
= <jgeddes@turing.ac.uk>
2022-12-01 09:41:09 +0000

    Racket: tidy up

--- day-01/racket_triangle-man/day01.rkt
+++ day-01/racket_triangle-man/day01.rkt
@@ -5,21 +5,21 @@
 
 ;; Given a list of strings, some of which are empty, split the list at the empty strings to produce a
 ;; list of lists
-(define (split-lines-on-blank xs)
+(define (split-on-blank xs)
   (let ([empty-string? (λ (x) (not (= (string-length x) 0)))])
     (if (null? xs)
         null
         (let-values
-            ([(elf elves) (splitf-at xs empty-string?)])
-          (if (null? elves)
-              (list elf)
-              (cons elf (split-lines-on-blank (cdr elves))))))))
+            ([(lst rest) (splitf-at xs empty-string?)])
+          (if (null? rest)
+              (list lst)
+              (cons lst (split-on-blank (cdr rest))))))))
 
 (define *input*
-  (let ([elves (~>
-                (with-input-from-file "input.txt" port->lines)
-                split-lines-on-blank)])
-    (map (curry map string->number) elves)))
+  (~>
+   (with-input-from-file "input.txt" port->lines)
+   split-on-blank
+   (map (curry map string->number) _)))
 
 ;; Part 1
 
