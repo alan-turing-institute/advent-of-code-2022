3621a2f2cfa6d6f73d7583f1535caf42f3ea1c1f
rchan <rchan@turing.ac.uk>
2022-12-06 08:29:53 +0000

    detect not decode?

--- day-06/julia_rchan26/day_six.jl
+++ day-06/julia_rchan26/day_six.jl
@@ -3,7 +3,7 @@ using Test
 test_input = readlines("test_input.txt")
 input = readlines("input.txt")[1]
 
-function decode_signal(input, n_unique)    
+function detect_signal(input, n_unique)    
     position = n_unique-1
     unique = 0
     while (unique != n_unique)
@@ -14,21 +14,21 @@ function decode_signal(input, n_unique)
 end
 
 # testing cases for Part One
-Test.@test decode_signal(test_input[1], 4) == (7, "jpqm")
-Test.@test decode_signal(test_input[2], 4) == (5, "vwbj")
-Test.@test decode_signal(test_input[3], 4) == (6, "pdvj")
-Test.@test decode_signal(test_input[4], 4) == (10, "rfnt")
-Test.@test decode_signal(test_input[5], 4) == (11, "zqfr")
+Test.@test detect_signal(test_input[1], 4) == (7, "jpqm")
+Test.@test detect_signal(test_input[2], 4) == (5, "vwbj")
+Test.@test detect_signal(test_input[3], 4) == (6, "pdvj")
+Test.@test detect_signal(test_input[4], 4) == (10, "rfnt")
+Test.@test detect_signal(test_input[5], 4) == (11, "zqfr")
 
 # testing cases for Part Two
-Test.@test decode_signal(test_input[1], 14) == (19, "qmgbljsphdztnv")
-Test.@test decode_signal(test_input[2], 14) == (23, "vbhsrlpgdmjqwf")
-Test.@test decode_signal(test_input[3], 14) == (23, "ldpwncqszvftbr")
-Test.@test decode_signal(test_input[4], 14) == (29, "wmzdfjlvtqnbhc")
-Test.@test decode_signal(test_input[5], 14) == (26, "jwzlrfnpqdbhtm")
+Test.@test detect_signal(test_input[1], 14) == (19, "qmgbljsphdztnv")
+Test.@test detect_signal(test_input[2], 14) == (23, "vbhsrlpgdmjqwf")
+Test.@test detect_signal(test_input[3], 14) == (23, "ldpwncqszvftbr")
+Test.@test detect_signal(test_input[4], 14) == (29, "wmzdfjlvtqnbhc")
+Test.@test detect_signal(test_input[5], 14) == (26, "jwzlrfnpqdbhtm")
 
 # Part One
-println("Part One: ", decode_signal(input, 4))
+println("Part One: ", detect_signal(input, 4))
 
 # Part Two
-println("Part Two: ", decode_signal(input, 14))
+println("Part Two: ", detect_signal(input, 14))
