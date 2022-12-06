9aceece9442f1cfe72b60161357e435fdf68a8ab
radka-j <r.jersakova@gmail.com>
2022-12-06 21:02:57 +0000

    add comments

--- day-06/python_radka-j/day06.py
+++ day-06/python_radka-j/day06.py
@@ -13,14 +13,17 @@ def evaluate(my_str, n_required):
     """
 
     for i, char in enumerate(my_str):
-        # do nothin until see n_required characters
+        # do nothing until have n_required characters
         if i <= n_required - 1:
             continue
         else:
+            # check number of unique characters in substring
             substring = my_str[i-n_required:i]
             n_unique = len(set(substring))
             if n_unique==n_required:
                 return i
+    # nothing found
+    return -1
 
 assert evaluate("bvwbjplbgvbhsrlpgdmjqwftvncz", 4) == 5
 assert evaluate("nppdvjthqldpwncqszvftbrmjlhg", 4) == 6
